5acd48ba8383b9abb6f1ca5c8a6f50716249a22f
AmirHosseinHasanloo <AmirHosseinHasanloo2005@gmail.com>
2025-07-29 00:45:18 +0330

    feat => Dispatcher health check url fixed up. --fix

--- SecureProcessor.Core/Patterns/Options/DispatcherOptions.cs
+++ SecureProcessor.Core/Patterns/Options/DispatcherOptions.cs
@@ -10,6 +10,6 @@ namespace SecureProcessor.Core.Patterns.Options
     {
         public int MaxActiveProcessors { get; set; } = 5;
         public int HealthCheckInterval { get; set; } = 30;
-        public string ManagerServiceUrl { get; set; } = "https://localhost:7138";
+        public string ManagerServiceUrl { get; set; } = "https://localhost:7138/api/Module/health";
     }
 }
